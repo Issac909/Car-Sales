222b41f9a738d246611a0c931e640bcd8d831c98
Issac Moreno <issaxavier909@gmail.com>
2020-02-18 14:41:48 -0800

    MVP-retake complete

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,35 +1,22 @@
-import React from "react";
-import AddedFeature from "./AddedFeature";
-import { connect } from "react-redux";
-// Connecting the component to the store
+import React from 'react';
 
-const AddedFeatures = ({ car, removeFeature }) => {
-    return (
-        <div className="content">
-            <h6>Added features:</h6>
-            {car.features.length ? (
-                <ol type="1">
-                    {car.features.map(item => (
-                        <AddedFeature
-                            key={item.id}
-                            feature={item}
-                            removeFeature={removeFeature}
-                        />
-                    ))}
-                </ol>
-            ) : (
-                <p>You can purchase items from the store.</p>
-            )}
-        </div>
-    );
+import AddedFeature from './AddedFeature';
+
+const AddedFeatures = props => {
+  return (
+    <div className="content">
+      <h6>Added features:</h6>
+      {props.car.features.length ? (
+        <ol type="1">
+          {props.car.features.map(item => (
+            <AddedFeature key={item.id} feature={item} removeFeature = {props.removeFeature} />
+          ))}
+        </ol>
+      ) : (
+        <p>You can purchase items from the store.</p>
+      )}
+    </div>
+  );
 };
 
-const mapStateToProps = state => {
-    return {
-        car: state.car
-    };
-};
-export default connect(
-    mapStateToProps,
-    {}
-)(AddedFeatures);
+export default AddedFeatures;
